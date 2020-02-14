da349b97baeba59da07065ceec500d7db37d814f
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-02-14 10:15:23 -0800

    project router done

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -2,4 +2,7 @@ const express = require('express');
 const actionDB = require('../helpers/actionModel');
 const router = express.Router();
 
+
+router.get("/")
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const projectRouter = require('./data/routers/projectRouter');
 const server = express();
 server.use(express.json());
 
-server.use('/api/actions', actionRouter);
-server.use('/api/projects', projectRouter);
+server.use('/api/:id/actions', logger, actionRouter);
+server.use('/api/projects', logger, projectRouter);
+
+server.get('/', (req, res) => {
+    res.send(
+        `<h2> Welcome to Node API Sprint Challenge! </h2>`
+    );
+});
 
 //global middleware
 
-
+function logger(req, res, next) {
+    console.log(`${req.method} request made to ${req.originalUrl}`);
+    next();
+}
 
 module.exports = server;
